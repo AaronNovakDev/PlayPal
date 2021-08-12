7ecfb1251b2ff68316deb904792de924b25b1b4f
Aaron <aaronnovakdev@gmail.com>
2021-08-12 14:51:02 +1000

    created basic list to work on for freinds also, no time at the mo

--- xplory_front/src/components/Filter.js
+++ xplory_front/src/components/Filter.js
@@ -4,21 +4,22 @@ const Filter = ()=>{
     return(
         <div className="filter">
             <p>This is the filter page , please enter what you would like to filter through</p>
-            <p><ul>
-           <li> Water - Potted/Unpotted</li>
-           <li> Pet freiendly</li>
-            <li>Shelter</li>
-            <li>Power</li>
-            <li>Lighting</li>
-            <li>Cooking facilities - Gas or Electric</li>
-            <li>Toilet</li>
-            <li>Shower - Hot or Cold</li>
-            <li>Parking </li>
-            <li>Wheelchair Access </li>
-           <li> Fenced off park for kids</li>
-            <li>Beach access</li>
-            <li>Events - DnD meetups</li>
-            </ul>
+            <p>
+                <ul>
+                  <li> Water - Potted/Unpotted</li>
+                  <li> Pet freiendly</li>
+                  <li>Shelter</li>
+                  <li>Power</li>
+                  <li>Lighting</li>
+                  <li>Cooking facilities - Gas or Electric</li>
+                  <li>Toilet</li>
+                  <li>Shower - Hot or Cold</li>
+                  <li>Parking </li>
+                  <li>Wheelchair Access </li>
+                  <li> Fenced off park for kids</li>
+                  <li>Beach access</li>
+                  <li>Events - DnD meetups</li>
+                 </ul>
             </p>
         </div>
     )
--- xplory_front/src/components/Friends.js
+++ xplory_front/src/components/Friends.js
@@ -5,6 +5,14 @@ const Friends = () => {
     return(
         <div className="friends">
             <p>This is your freinds page! Please click on your freinds to find out where they visitted.</p>
+            <p>
+                <ul>
+                  <li>David Attenborough</li>
+                  <li>Bruce Lee</li>
+                  <li>Barrack Obama</li>
+                  <li>Bobby Brown</li>
+                 </ul>
+            </p>
         </div>
     )
 }
