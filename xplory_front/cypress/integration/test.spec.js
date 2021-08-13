a7dfaada0a566c3d4d4006a854dd2fea7b373cca
Aaron <aaronnovakdev@gmail.com>
2021-08-13 17:14:25 +1000

    trying to get cypress to WORKgit add .!

--- xplory_front/cypress/integration/test.spec.js
+++ xplory_front/cypress/integration/test.spec.js
@@ -8,22 +8,52 @@
 
 describe('Check element login', () => {
     it('finds the content "Email"', () => {
-     cy.visit('https://xploryapp.herokuapp.com/LoginForm')
+     cy.visit('https://xploryapp.herokuapp.com/signup')
 
      cy.contains('Email')
     })
 
+ describe('new user signup', () => {
+beforeEach(() => {
+    cy.visit("https://xploryapp.herokuapp.com/signup")
+})
 
-    describe('new user signup', () => {
     it('creates a new user"', () => {
-        cy.visit('https://xploryapp.herokuapp.com/LoginForm')
+        // cy.visit('https://xploryapp.herokuapp.com/signup')
 
-        cy.get("[data-cy=signup-username]".type("NewUser")
-        cy.get("[data-cy=signup-email]".type("NewUser@test.com")
-        cy.get("[data-cy=signup-password]".type("Abcd1234")
-        cy.get("[data-cy=signup-passwordConfirmation]".type("Abcd1234")
+        cy.get("[data-cy=signup-username]").type("NewUser" + Date.now())
+        cy.get("[data-cy=signup-email]").type("Newuser@test.com" + Date.now())
+        cy.get("[data-cy=signup-password]").type("Abcd1234")
+        cy.get("[data-cy=signup-passwordConfirmation]").type("Abcd1234")
 
-        cy.get("[data-cy=signup-button]")
+        cy.get("[data-cy=signup-button]").click()
 
- })
+        cy.url().should('eq', Cypress.config().baseUrl + "/")
+    })
+
+
+    describe('When NOT in Chrome', { browser: '!chrome' }, () => {
+        it('Shows warning', () => {
+            cy.get('.browser-warning').should(
+                'contain',
+                'For optimal viewing, use Chrome Browser please!'
+            )
+        })
+        it('Links to browser compatibility doc', () => {
+            cy.get('a.browser-compat')
+              .should('have.attr', 'href')
+              .and('include', 'browser-compatibility')
+          })
+        })
+
+
+        it("requires a username", () => {
+
+            cy.get("[data-cy=signup-email]").type("Newuser@test.com" + Date.now())
+            cy.get("[data-cy=signup-password]").type("Abcd1234")
+            cy.get("[data-cy=signup-passwordConfirmation]").type("Abcd1234")
+
+            cy.get("[data-cy=signup-button]").should("be.disabled")
+        })
+    })
 })
